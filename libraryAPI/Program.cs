e31cfe5b153090d47125863efb09575add30565c
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2022-01-23 23:40:52 +0530

    feat(): Swagger UI included

--- libraryAPI/Program.cs
+++ libraryAPI/Program.cs
@@ -1,5 +1,22 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// create builder service for Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "Library API", Description = "Keeping track of the Books of a library"});
+});
+
 var app = builder.Build();
+
+// create Swagger UI
+app.UseSwagger();
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Library API v1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
